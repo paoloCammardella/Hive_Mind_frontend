5ff019a7b93303c5f3da04d5a4b26b024835fae5
paoloCammardella <po.cammardella@gmail.com>
2024-06-10 14:28:53 +0200

    Ho inserito le tabs e modificato la navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { ThemePalette } from '@angular/material/core';
 import {MatProgressBar, ProgressBarMode} from '@angular/material/progress-bar'
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import { RouterOutlet } from '@angular/router';
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatProgressBar],
+  imports: [MatProgressBar, MatButtonModule, MatMenuModule, MatIconModule, MatTabsModule, RouterOutlet],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   bufferValue: number = 100;
-  mode: ProgressBarMode = "query";
+  mode: ProgressBarMode = "determinate";
   color: ThemePalette = "#F8B828" as ThemePalette;
 }
