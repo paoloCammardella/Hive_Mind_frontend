d162a9fb73b5265ce5246aaee076bf97e58a2f0f
paoloCammardella <po.cammardella@gmail.com>
2024-06-14 17:41:05 +0200

    separato navBar e footer dal login e signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,21 +2,34 @@ import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { LandingComponent } from './landing/landing.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "login"
+  },
   {
     path: "signup",
     component: SignupComponent,
-    title: "Sign Up"
+    title: "Sign Up | Hive Mind"
   },
   {
     path: "login",
     component: LoginComponent,
-    title: "Log in"
+    title: "Log in | Hive Mind"
   },
   {
     path: "",
-    component: LandingComponent,
-    title: "Hive Mind"
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      }
+    ]
   }
 ];
---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [NavbarComponent, FooterComponent, RouterOutlet],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent {
+
+}
