106f3c995ecc926147ca1fd0a39c5e8e7a366f4f
paoloCammardella <po.cammardella@gmail.com>
2024-06-19 11:11:37 +0200

    controllare il CSS da capo, partendo dal signup e passando poi al login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
-import { LandingComponent } from './landing/landing.component';
 import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './home/home.component';
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 export const routes: Routes = [
 
@@ -22,10 +20,6 @@ export const routes: Routes = [
     path: "login",
     component: LoginComponent,
     title: "Log in | Hive Mind"
-  }, {
-    path: "forgotpassword",
-    component: ForgotPasswordComponent,
-    title: "Forgot password | Hive Mind"
   },
   {
     path: "",
