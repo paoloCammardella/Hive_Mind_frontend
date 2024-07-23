d7df428addd886324647767e3699526ba148f1e4
paoloCammardella <po.cammardella@gmail.com>
2024-07-23 23:58:19 +0200

    aggiunto il salvataggio del login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { EditorModule } from 'primeng/editor';
 import { SignupComponent } from './pages/signup/signup.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TokenInterceptorService } from './_services/JWT/token-interceptor-service.service';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthService } from './_services/auth/auth.service';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  providers:  [],
+  providers: [],
   imports: [RouterOutlet, EditorModule, SignupComponent, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Hive_Mind_frontend';
-  text = 'prova';
+
+//TODO: provare token scaduto
+  authService = inject(AuthService);
+  router = inject(Router);
+  ngOnInit(): void {
+    if (this.authService.verifyToken(localStorage.getItem('token'))) {
+      this.router.navigateByUrl("/home");
+    }
+  }
 }
